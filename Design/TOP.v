2cdf29bec113665c58d73076da63d1ecf74199a5
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-08-12 01:25:51 +0300

    TOP Module has been Created

---
+++ Design/TOP.v
@@ -0,0 +1,11 @@
+module SYS_TOP(
+    // Clocks and Resets
+    input wire REF_CLK,
+    input wire UART_CLK,
+    input wire RST,
+    //UART
+    input wire RX_IN,
+    output wire TX_OUT 
+);
+
+endmodule
