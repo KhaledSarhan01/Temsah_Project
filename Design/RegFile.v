978222eca58aa04779568f4dfebf941c2d768017
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-08-12 23:34:05 +0300

    RegFile is added and Integrated to the System + Added to the Flow

---
+++ Design/RegFile.v
@@ -0,0 +1,70 @@
+// 8x16 Register file with read/write enable 
+module RegFile #(parameter DATA_WIDTH = 8, parameter MEM_SIZE = 16 ,parameter ADDR_WIDTH = 4) (
+    //Clock and Active Low Reset
+    input   wire CLK,RST,
+    //Datapath
+    input   wire [DATA_WIDTH-1:0] WrData,         
+    output  reg [DATA_WIDTH-1:0] RdData,         
+    //Control
+    input   wire [ADDR_WIDTH-1:0]   Address,        
+    input   wire WR_En ,RD_EN,
+    output  reg RdData_Vaild,
+    //Internal Registers
+    output  wire [DATA_WIDTH-1:0] REG0, // ALU INPUT 
+    output  wire [DATA_WIDTH-1:0] REG1, // ALU INPUT
+    output  wire [DATA_WIDTH-1:0] REG2, // UART Configuraion
+    output  wire [DATA_WIDTH-1:0] REG3 // Clock Divider  
+);
+
+//Default Values of important Registers
+localparam REG0_Defualt = 'b0,
+           REG1_Defualt = 'b0, 
+           REG2_Defualt = {5'd32,1'b0,1'b1}, // Prescale: REG[7:2]=32, Parity Type:REG[1]=0 ,Parity Enable:REG[0] = 1  
+           REG3_Defualt = 'd32;              // Prescale: REG[7:0]=32 
+
+//Memory Assignment 
+    reg [DATA_WIDTH-1:0] Register [MEM_SIZE-1:0];
+
+//Important Variables    
+    integer i;
+
+//sequentail block
+always @(posedge CLK or negedge RST) begin
+    if(!RST)begin
+        Register[0] <= REG0_Defualt;
+        Register[1] <= REG1_Defualt;
+        Register[2] <= REG2_Defualt;
+        Register[3] <= REG3_Defualt;
+
+        for ( i = 4 ; i<= MEM_SIZE-1 ;i=i+1 ) begin
+            Register[i] <= {DATA_WIDTH{1'b0}};
+        end
+
+        RdData_Vaild <= 1'b0;
+    end 
+    else begin
+        if(WR_En && !(RD_EN))begin
+            Register[Address] <= WrData ;
+            RdData_Vaild <= 1'b0;
+        end 
+        else if (!(WR_En) && RD_EN) begin
+            RdData <= Register[Address];
+            RdData_Vaild <= 1'b1;
+        end
+        else begin
+            RdData <= 16'b0;
+            RdData_Vaild <= 1'b0;
+        end 
+    end
+end
+
+//Outputs
+  //  assign RdData = (!(WR_En) && RD_EN)? Register[Address]:'b0;
+  // assign RdData_Vaild = (!(WR_En) && RD_EN);
+
+    assign REG0 = Register[0];
+    assign REG1 = Register[1];
+    assign REG2 = Register[2];
+    assign REG3 = Register[3];
+
+endmodule
--- Design/TOP.v
+++ Design/TOP.v
@@ -9,9 +9,10 @@ module SYS_TOP #(parameter DATA_WIDTH = 8)(
 );
 
 ////--------- Internal Signals ---------////
-    // System Control
+    // System Control Datapath
     wire [DATA_WIDTH-1:0] SYS_UART_TX_IN , UART_TX_IN;
-    
+    wire [DATA_WIDTH-1:0] RegFile_WORD_IN,RegFile_WORD_OUT;
+
     // FIFO Control and Flags
     wire F_RD_INC,F_WR_INC;
     wire F_FULL,F_EMPTY;
@@ -19,17 +20,51 @@ module SYS_TOP #(parameter DATA_WIDTH = 8)(
     // Clocks
     wire TX_CLK;
 
+    // ALU Datapath and Control
+    wire [DATA_WIDTH-1:0] ALU_Op1,ALU_Op2;
+
+    //Register File Control and Flags
+    wire RegFile_Rd_En,RegFile_Wr_En;
+    wire RegFile_Data_Vaild;
+
     // Synchronizers
     wire RST_SYNC_REF,RST_SYNC_UART;
 
+    // Configuration Signals
+    wire [DATA_WIDTH-1:0] UART_Config,ClkDiv_Config;
+
 ////---------- Clock Domain 1 ----------////
+
+// Register File
+    parameter  RegFile_MEM_SIZE   = 4 ;
+    parameter  RegFile_ADDR_WIDTH = 16;
+    RegFile #(.DATA_WIDTH(DATA_WIDTH),.MEM_SIZE(RegFile_MEM_SIZE) ,.ADDR_WIDTH(RegFile_ADDR_WIDTH)) Register_File (
+    //Clock and Active Low Reset
+    .CLK(REF_CLK),
+    .RST(RST_SYNC_REF),
+    //Datapath
+    .WrData(RegFile_WORD_IN),         
+    .RdData(RegFile_WORD_OUT),         
+    //Control
+    .Address(RegFile_ADDR),        
+    .WR_En(RegFile_Wr_En),
+    .RD_EN(RegFile_Rd_En),
+    .RdData_Vaild(RegFile_Data_Vaild),
+    //Internal Registers
+    .REG0(ALU_Op1), // ALU INPUT 
+    .REG1(ALU_Op2), // ALU INPUT
+    .REG2(UART_Config), // UART Configuraion
+    .REG3(ClkDiv_Config) // Clock Divider  
+);
 
 ////---------- Clock Domain 2 ----------////
 
 ////---------- Synchronizers -----------////
-    parameter ADDR_WIDTH = 4;
-    parameter MEM_SIZE = 32;
-    FIFO #(.DATA_WIDTH(DATA_WIDTH),.ADDR_WIDTH(ADDR_WIDTH),.MEM_SIZE(MEM_SIZE)) Async_FIFO (
+
+//FIFO    
+    parameter FIFO_ADDR_WIDTH = 4;
+    parameter FIFO_MEM_SIZE = 32;
+    FIFO #(.DATA_WIDTH(DATA_WIDTH),.ADDR_WIDTH(FIFO_ADDR_WIDTH),.MEM_SIZE(FIFO_MEM_SIZE)) Async_FIFO (
         //Write Part
         .W_CLK(REF_CLK),
         .W_RST(RST_SYNC_REF),
